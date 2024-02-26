0a14d62cf3fa8365a162854ad6f5e002b64269ea
Akhil Kothapalli <60518974+kakhil2004@users.noreply.github.com>
2024-02-26 17:02:02 -0500

    first full fix

---
+++ .svelte-kit/output/client/_app/immutable/chunks/paths.Fha7R2bz.js
@@ -0,0 +1,1 @@
+var s;const o=((s=globalThis.__sveltekit_oxvlso)==null?void 0:s.base)??"/simpleport2";var e;const t=((e=globalThis.__sveltekit_oxvlso)==null?void 0:e.assets)??o;export{t as a,o as b};
---
+++ build/_app/immutable/entry/start.B6XQMthv.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CuVmJ_oA.js";export{t as start};
