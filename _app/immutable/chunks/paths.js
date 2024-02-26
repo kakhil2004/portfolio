749e66471762b552c8541a0300a4b2a955626c1f
Akhil Kothapalli <60518974+kakhil2004@users.noreply.github.com>
2024-02-26 03:35:35 -0500

    Updates

--- _app/immutable/chunks/paths.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_rupme6)==null?void 0:s.base)??"/simpleport2";var e;const a=((e=globalThis.__sveltekit_rupme6)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.DIBt8b15.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Cfx9f-7l.js";export{t as start};
