5a01680c033cd296be077fcd06345438d147f935
KilHak <60518974+KilHak@users.noreply.github.com>
2024-03-29 13:59:35 -0400

    Updates

---
+++ _app/immutable/chunks/paths.Dkjtmrhc.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1mqd3ao)==null?void 0:s.base)??"/simpleport2";var a;const t=((a=globalThis.__sveltekit_1mqd3ao)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.B9GX3iCn.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DdfpaJFI.js";export{t as start};
